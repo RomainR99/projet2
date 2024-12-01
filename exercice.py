549c596a2283415965141b193cad48ff7ae29b6b
Romain <romain.rothn@gmail.com>
2024-12-01 19:08:55 +0100

    merge lister les todo

--- exercice.py
+++ exercice.py
@@ -3,3 +3,14 @@ def creer_todo():
     titre = input("Entrez le titre du todo : ")
     todos.append((titre, "À faire"))
     print(f"Todo '{titre}' ajouté avec le statut 'À faire'.")
+# Liste pour stocker les todos et leurs statuts
+todos = []
+
+# Fonction pour lister les todos
+def lister_todos():
+    if not todos:
+        print("Aucun todo à afficher.")
+    else:
+        print("\nListe des todos :")
+        for i, (titre, statut) in enumerate(todos, 1):
+            print(f"{i}. {titre} - {statut}")
