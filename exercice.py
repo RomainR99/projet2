04472634a366b3ce211ce6a9c7751cd0d11bd85b
Romain <romain.rothn@gmail.com>
2024-12-01 20:08:44 +0100

    hotfix-1

--- exercice.py
+++ exercice.py
@@ -29,7 +29,7 @@ lister_todos()
                 print(f"Le statut du todo '{titre}' a été mis à jour à 'Fait'.")
             elif statut == "Fait":
                 todos[choix] = (titre, "À fair")  # Erreur volontaire
-                print(f"Le statut du todo '{titre}' a été mis à jour à 'À fair'.")
+                print(f"Le statut du todo '{titre}' a été mis à jour à 'À faire'.")
             else:
                 print(f"Statut inconnu pour le todo '{titre}'. Aucune modification effectuée.")
         else:
